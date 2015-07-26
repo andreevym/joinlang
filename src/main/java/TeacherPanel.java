59884c1bf45bf8bf748e2ff8dd444e37859bf3aa
Юра <andreevym@gmail.com>
2015-07-27 00:37:32 +0300

    web
    gradle, jetty, war

--- src/main/java/TeacherPanel.java
+++ src/main/java/TeacherPanel.java
@@ -1,12 +1,10 @@
 /**
  * Created by yandreev on 25.07.2015.
  */
-import org.joda.time.LocalTime;
+//import org.joda.time.LocalTime;
 
 public class TeacherPanel {
     public static void main(String[] args) {
-        LocalTime localTime = new LocalTime();
-        System.out.println("localTime" + localTime);
-        System.out.println("currentTime" + localTime);
+        System.out.println("localTime");
     }
 }
